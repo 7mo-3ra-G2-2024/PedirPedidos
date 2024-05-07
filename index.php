4b021d81ecb5261c8d5b74b954f0074f1ab201d6
Rusiton <galassosantiago05@gmail.com>
2024-05-06 21:33:52 -0300

    Cambiado el tipo de conexión a la bd (ahora es para sql)

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php 
-    require('conexion.php');
-    include 'navbar.php';
-    include 'carrito.php';
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -12,20 +6,36 @@
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-
+<?php 
+    require 'conexion.php';
+    include 'navbar.php';
+    include 'carrito.php';
+?>
 <h1 class="title">Productos Disponibles</h1>
     
 <div class="productos-container">
     <?php
-        $productos_json = file_get_contents("productos.json");
-        $productos = json_decode($productos_json, true);
+        // $productos_json = file_get_contents("productos.json");
+        // $productos = json_decode($productos_json, true);
         
-        foreach ($productos as $producto) {
+        // foreach ($productos as $producto) {
+        //     echo "<div class='producto'>";
+        //     echo "<h2>" . $producto['nombre'] . "</h2>";
+        //     echo "<p>Precio: $" . $producto['precio'] . "</p>";
+        //     // Agregar un botón con un identificador único para cada producto
+        //     echo "<button onclick='agregarAlCarrito(\"" . $producto['nombre'] . "\", " . $producto['precio'] . ")'>Agregar al Carrito</button>";
+        //     echo "</div>";
+        // }
+
+        $request = $conexion->prepare("SELECT * FROM item");
+        $consulta->execute();
+        $data = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($data as $element){
             echo "<div class='producto'>";
-            echo "<h2>" . $producto['nombre'] . "</h2>";
-            echo "<p>Precio: $" . $producto['precio'] . "</p>";
-            // Agregar un botón con un identificador único para cada producto
-            echo "<button onclick='agregarAlCarrito(\"" . $producto['nombre'] . "\", " . $producto['precio'] . ")'>Agregar al Carrito</button>";
+            echo "<h2>".$element['nombre']."</h2>";
+            echo "<p>Precio : $".$element['precio']."</p>";
+            echo "<button onclick='agregarAlCarrito(\"". $element['name']."\",'".$element['precio'].")'>Agregar al Carrito</button>";
             echo "</div>";
         }
     ?>
