92a521e23ab380ee18db6289987821f161dd41a5
Demeck <128102337+Demeckk@users.noreply.github.com>
2024-05-20 19:01:40 -0300

    Add files via upload

--- componentes/registros/registro.php
+++ componentes/registros/registro.php
@@ -1,18 +1,18 @@
 <?php
   /* require('conexion.php'); */
-  include '/PedirPedidos 2.0/componentes/navbar/navbar.php'; 
+  include '../navbar/navbar.php'; 
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>Registro de Usuario</title>
-    <link rel="stylesheet" href="/PedirPedidos 2.0/css/estilos.css">
+    <link rel="stylesheet" href="../../css/estilos.css">
 </head>
 <body>
     <h1>Registro de Usuario</h1>
     <p>Por favor, selecciona tu tipo de usuario:</p>
-    <a href="/PedirPedidos 2.0/componentes/registros/registro_cliente.php" class="btn-register">Registrarse como Cliente</a>
-    <a href="/PedirPedidos 2.0/componentes/registros/registro_repartidor.php" class="btn-register">Registrarse como Repartidor</a>
+    <a href="registro_cliente.php" class="btn-register">Registrarse como Cliente</a>
+    <a href="registro_repartidor.php" class="btn-register">Registrarse como Repartidor</a>
 </body>
 </html>
