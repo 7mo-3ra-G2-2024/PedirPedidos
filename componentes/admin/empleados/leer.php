85752500c8d09467475eb0ae7e403e1a9ffc626f
Rusiton <galassosantiago05@gmail.com>
2024-06-24 14:06:30 -0300

    REFACTOR modificado el codigo de los archivos

--- componentes/admin/empleados/leer.php
+++ componentes/admin/empleados/leer.php
@@ -7,7 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-    <center><table border="1" style="margin: 10px;"><tr><th>ID Empleado</th><th>Nombre</th><th>Apellido</th><th>DNI</th></tr>
+    <center><table border="1" style="margin: 10px;">
+        <tr><th>ID Empleado</th><th>Nombre</th><th>Apellido</th><th>DNI</th><th>E-Mail</th><th>Teléfono</th></tr>
     <?php
         require "../../../conexion.php";
         include "../../cliente/navbar/navbar.php";
@@ -18,7 +19,7 @@
         $data = $query->fetchAll(PDO::FETCH_ASSOC);
 
         foreach($data as $row){
-            echo "<tr><td>".$row['idEmpleado']."<td>".$row['nombre']."</td><td>".$row['apellido']."</td><td>".$row['DNI']."</td></tr>";
+            echo "<tr><td>".$row['idEmpleado']."<td>".$row['nombre']."</td><td>".$row['apellido']."</td><td>".$row['dni']."</td><td>".$row['email']."</td><td>".$row['telefono']."</td></tr>";
         }
     ?>
     </table></center>
